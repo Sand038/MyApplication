751158c69a2e89ff033329a9e24ac837cb851461
SFernando <sandmdh@gmail.com>
2022-06-12 13:38:46 +0530

    added sematext integration

--- app/src/main/java/com/sand/myapplication/MainActivity.java
+++ app/src/main/java/com/sand/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sand.myapplication;
 
+import com.sematext.logseneandroid.Logsene;
+
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,15 +9,20 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
   int counter = 1;
+  Logsene logsene;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Logsene.init(this);
+    logsene = Logsene.getInstance();
   }
 
   public void sendMessage(View view) {
-    System.out.println("The counter value is: " + counter++);
+    String log = "The counter value is: " + counter++;
+    System.out.println(log);
+    logsene.info(log);
   }
 
 }
